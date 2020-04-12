c074abd4e87ee72ca54f3fd7e2bc41a6c16d13b3
dgwight <dylan@otechie.com>
2020-04-11 21:07:52 -0700

    initial

--- src/main.js
+++ src/main.js
@@ -4,63 +4,62 @@ import { show } from './views/message'
 const supportedAPI = ['init', 'message']; // enlist all methods supported by API (e.g. `mw('event', 'user-login');`)
 
 /**
-    The main entry of the application
-    */
-function app(window) {
-    console.log('JS-Widget starting');
+ The main entry of the application
+ */
+function app (window) {
+  console.log('JS-Widget starting');
 
-    // set default configurations
-    let configurations = {
-        someDefaultConfiguration: false
-    };
+  // set default configurations
+  let configurations = {
+    someDefaultConfiguration: false
+  };
 
-    // all methods that were called till now and stored in queue
-    // needs to be called now 
-    let globalObject = window[window['JS-Widget']];
-    let queue = globalObject.q;
-    if (queue) {
-        for (var i = 0; i < queue.length; i++) {
-            if (queue[i][0].toLowerCase() == 'init') {
-                configurations = extendObject(configurations, queue[i][1]);
-                console.log('JS-Widget started', configurations);
-            }
-            else
-                apiHandler(queue[i][0], queue[i][1]);
-        }
+  // all methods that were called till now and stored in queue
+  // needs to be called now
+  let globalObject = window[window['JS-Widget']];
+  let queue = globalObject.q;
+  if (queue) {
+    for (var i = 0; i < queue.length; i++) {
+      if (queue[i][0].toLowerCase() == 'init') {
+        configurations = extendObject(configurations, queue[i][1]);
+        console.log('JS-Widget started', configurations);
+      } else
+        apiHandler(queue[i][0], queue[i][1]);
     }
+  }
 
-    // override temporary (until the app loaded) handler
-    // for widget's API calls
-    globalObject = apiHandler;
-    globalObject.configurations = configurations;
+  // override temporary (until the app loaded) handler
+  // for widget's API calls
+  globalObject = apiHandler;
+  globalObject.configurations = configurations;
 }
 
 /**
-    Method that handles all API calls
-    */
-function apiHandler(api, params) {
-    if (!api) throw Error('API method required');
-    api = api.toLowerCase();
+ Method that handles all API calls
+ */
+function apiHandler (api, params) {
+  if (!api) throw Error('API method required');
+  api = api.toLowerCase();
 
-    if (supportedAPI.indexOf(api) === -1) throw Error(`Method ${api} is not supported`);
+  if (supportedAPI.indexOf(api) === -1) throw Error(`Method ${api} is not supported`);
 
-    console.log(`Handling API call ${api}`, params);
+  console.log(`Handling API call ${api}`, params);
 
-    switch (api) {
-        // TODO: add API implementation
-        case 'message':
-            show(params);
-            break;
-        default:
-            console.warn(`No handler defined for ${api}`);
-    }
+  switch (api) {
+    // TODO: add API implementation
+    case 'message':
+      show(params);
+      break;
+    default:
+      console.warn(`No handler defined for ${api}`);
+  }
 }
 
-function extendObject(a, b) {
-    for (var key in b)
-        if (b.hasOwnProperty(key))
-            a[key] = b[key];
-    return a;
+function extendObject (a, b) {
+  for (var key in b)
+    if (b.hasOwnProperty(key))
+      a[key] = b[key];
+  return a;
 }
 
 app(window);
--- src/views/message.js
+++ src/views/message.js
@@ -2,13 +2,16 @@ import html from './message.html';
 import './message.css';
 
 let elements = [];
+let chat;
+let chatbox;
 let body;
+let textarea;
 
 export function show(text) {
     // convert plain HTML string into DOM elements
     let temporary = document.createElement('div');
     temporary.innerHTML = html;
-    temporary.getElementsByClassName('js-widget-dialog')[0].textContent = text;
+    // temporary.getElementsByClassName('js-widget-dialog')[0].textContent = text;
 
     // append elements to body
     body = document.getElementsByTagName('body')[0];
@@ -16,13 +19,15 @@ export function show(text) {
         elements.push(temporary.children[0]);
         body.appendChild(temporary.children[0]);
     }
-
-    body.addEventListener('click', close);
+    chat = document.getElementsByClassName('otechie-widget-chat')[0];
+    chat.addEventListener('click', open);
 }
 
-export function close() {
-    while (elements.length > 0) {
-        elements.pop().remove();
-    }
-    body.removeEventListener('click', close);
+export function open() {
+    chat = document.getElementsByClassName('otechie-widget-chat')[0];
+    chatbox = document.getElementsByClassName('otechie-widget-chatbox')[0];
+    textarea = document.getElementsByClassName('otechie-widget-textarea')[0];
+    chatbox.hidden = !chatbox.hidden
+    chat.active = true
+    textarea.focus()
 }
