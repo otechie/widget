c557b0650cab8512fb2891ea6f55071b65ce04ac
dgwight <dylan@otechie.com>
2020-04-14 12:37:57 -0700

    ci

--- src/main.js
+++ src/main.js
@@ -3,12 +3,21 @@ import { show } from './views/message'
 function app (window) {
   // set default configurations
   let configurations = {
-    username: 'otechie-team'
+    username: 'otechie-team',
+    dev: false
   };
 
   // all methods that were called till now and stored in queue
   // needs to be called now
-  let globalObject = window[window['JS-Widget']];
+  let globalObject = window[window['Otechie-Widget']];
+  let queue = globalObject.q;
+  if (queue) {
+    for (var i = 0; i < queue.length; i++) {
+      if (queue[i][0].toLowerCase() == 'init') {
+        configurations = extendObject(configurations, queue[i][1]);
+      }
+    }
+  }
 
   // override temporary (until the app loaded) handler
   // for widget's API calls
--- src/views/message.js
+++ src/views/message.js
@@ -9,38 +9,42 @@ let body;
 let textarea;
 let x;
 let icon;
-let input
 let workspace = {};
+let API_URL;
+let WEB_URL;
 
 export function show () {
-  axios.get('http://localhost:8000/workspace/nuxt').then(result => {
-    workspace = result.data
-    document.getElementsByClassName('otechie-widget-workspace-name')[0].textContent = workspace.name;
-    document.getElementsByClassName('otechie-widget-workspace-rate')[0].textContent = `$${workspace.hourlyRate / 100}.00 / hr`;
-    document.getElementsByClassName('otechie-widget-avatar')[0].src = workspace.avatarUrl;
-    input.placeholder = `Start chat with ${workspace.name}`;
-  })
-  let temporary = document.createElement('div');
-  temporary.innerHTML = html;
+  API_URL = window.ow.configurations.dev ? 'http://api.dev-otechie.com' : 'http://api.otechie.com'
+  WEB_URL = window.ow.configurations.dev ? 'http://dev-otechie.com' : 'http://otechie.com'
 
   // append elements to body
   body = document.getElementsByTagName('body')[0];
+  let temporary = document.createElement('div');
+  temporary.innerHTML = html;
   while (temporary.children.length > 0) {
     elements.push(temporary.children[0]);
     body.appendChild(temporary.children[0]);
   }
-  chat = document.getElementsByClassName('otechie-widget-chat')[0];
-  chat.addEventListener('click', toggle);
-  input = document.getElementsByClassName('otechie-widget-textarea')[0]
-  input.addEventListener('onkeydown', send);
-}
 
-export function toggle () {
   chat = document.getElementsByClassName('otechie-widget-chat')[0];
   chatbox = document.getElementsByClassName('otechie-widget-chatbox')[0];
   textarea = document.getElementsByClassName('otechie-widget-textarea')[0];
   x = document.getElementsByClassName('otechie-widget-x')[0];
   icon = document.getElementsByClassName('otechie-widget-icon')[0];
+
+  textarea.addEventListener('keypress', keypress);
+  chat.addEventListener('click', toggle);
+
+  axios.get(`${API_URL}/workspace/${window.ow.configurations.username}`).then(result => {
+    workspace = result.data
+    document.getElementsByClassName('otechie-widget-workspace-name')[0].textContent = workspace.name;
+    document.getElementsByClassName('otechie-widget-workspace-rate')[0].textContent = `$${workspace.hourlyRate / 100}.00 / hr`;
+    document.getElementsByClassName('otechie-widget-avatar')[0].src = workspace.avatarUrl;
+    textarea.placeholder = `Start chat with ${workspace.name}`;
+  })
+}
+
+export function toggle () {
   chatbox.hidden = !chatbox.hidden
   icon.hidden = !icon.hidden
   x.hidden = !x.hidden
@@ -48,8 +52,9 @@ export function toggle () {
   textarea.focus()
 }
 
-export function send () {
-
-  console.log('input.textContent', input.textContent)
-  window.location.href = `https://dev-otechie.com/${workspace.username}?message=${input.textContent}`
+export function keypress (event) {
+  if (event.keyCode === 13) {
+    console.log('input.textContent', textarea.value)
+    window.location.href = `${WEB_URL}/${workspace.username}?message=${textarea.value}`
+  }
 }
